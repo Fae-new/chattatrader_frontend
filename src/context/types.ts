63e3a93e0941e1dbbca547f21d686028a3ebf420
karopeter <karopeter531@gmail.com>
2025-06-19 03:51:19 -0700

    updated fix

--- src/context/types.ts
+++ src/context/types.ts
@@ -5,7 +5,6 @@ export interface User {
   [key: string]: any;
   password: string;
   walletAddress: string;
-  otpVerified?: boolean;
   ethPrivateKey: string;
   ethMnemonic: string;
   baseholding: {
